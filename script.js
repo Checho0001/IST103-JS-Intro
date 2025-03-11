976cc3ab617f44441a8d24a218e2c4f5c7739988
Checho0001 <sverbar@gmail.com>
2025-03-10 20:22:10 -0700

    Sergio - Class Activity 2 - 10 March 2025 #1

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-alert("Hello World")
-function clickheading() {
-    console.log("Heading Clicked")
-    alert("Heading Clicked")
-}
+// alert("Hello World")
+// function clickheading() {
+//     console.log("Heading Clicked")
+//     alert("Heading Clicked")
+// }
+
+let Name = 'Sergio Vera'
+let NumberVar = 27
+let BoolVal = true
+let Undefinedvar = undefined
+let nullvar = null
+
+const ConstantVar = "static" + Name
+
